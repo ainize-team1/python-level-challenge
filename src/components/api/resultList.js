fa31487630fd5796ba9a5ffe3f51e634bb9f0fdd
dongcheol <dongcheol@comcom.ai>
2020-04-10 23:53:18 +0900

    add resultList

---
+++ src/components/api/resultList.js
@@ -0,0 +1,31 @@
+const resultList = [
+    // score 0
+    [{
+        name: "Alan Turing",
+        imgSrc: "https://upload.wikimedia.org/wikipedia/commons/a/a1/Alan_Turing_Aged_16.jpg",
+        description: "Alan Mathison Turing OBE FRS (/ˈtjʊərɪŋ/; 23 June 1912 – 7 June 1954) was an English mathematician, computer scientist, logician, cryptanalyst, philosopher, and theoretical biologist.",
+    }],
+    // score 1
+    [{}],
+    // score 2
+    [{}],
+    // score 3
+    [{}],
+    // score 4
+    [{}],
+    // score 5
+    [{}],
+    // score 6
+    [{}],
+    // score 7
+    [{}],
+    // score 8
+    [{}],
+    // score 9
+    [{}],
+    // score 10
+    [{}],
+]
+
+
+export default resultList
