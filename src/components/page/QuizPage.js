0dc1743e6924c3929f808650299c92bc3d9faf91
minsulee2 <minsu@comcom.ai>
2020-04-10 16:35:07 +0900

    Init Quiz page

--- src/components/page/QuizPage.js
+++ src/components/page/QuizPage.js
@@ -2,20 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-    flex: 1;
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
-    padding: 80px 0;
+    width: 360px;
+    background-color: #123123;
 `;
 
-const InfoText = styled.div`
+const Header = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+const CountText = styled.div`
     margin-top: ${props => props.marginTop || 0}px;
-    font-family: Roboto;
+    margin-left: ${props => props.marginTop || 0}px;
+    font-family: IBM Plex Sans;
+    font-style: normal;
+    font-weight: bold;
     font-size: 14px;
-    line-height: 1.71;
-    text-align: center;
-    color: #ffffff;
+    line-height: 18px;
+    letter-spacing: 0.15px;
+    color: #f2f2f2;
 `;
 
 class Quiz extends React.Component {
@@ -23,15 +34,20 @@ class Quiz extends React.Component {
         super();
 
         this.state = {
+            count: 1,
         };
     }
 
     render() {
+        const { count } = this.state;
+
         return (
             <Wrapper>
-                <InfoText marginTop={20}>
-                    {'quiz'}
-                </InfoText>
+                <Header>
+                    <CountText marginLeft={24} marginTop={24}>
+                        {`${count}/10`}
+                    </CountText>
+                </Header>
             </Wrapper>
         );
     }
