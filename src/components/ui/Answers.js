1c37a9a909676acad48ba2efaaa2fb5c218a673c
dongcheol <dongcheol@comcom.ai>
2020-04-13 19:30:56 +0900

    update resultpage(indant, tag, files)

--- src/components/ui/Answers.js
+++ src/components/ui/Answers.js
@@ -1,6 +1,29 @@
 import React from 'react';
-import Answer from './Answer'
+import styled from 'styled-components'
+import {FiCheckCircle} from 'react-icons/fi'
+import {FaRegTimesCircle} from 'react-icons/fa'
 
+const Grid = styled.div`  
+    display: grid;
+    width: 100%;
+    max-width: 300px;
+    background: #191919;
+    grid-template-columns: 40px auto 40px;    
+`;
+
+const Row = styled.div`
+    margin-left : 10px;
+    font-size : 12px;
+    text-align: left;
+    padding-top : 2px;
+`;
+
+
+const ShowAnswerList = styled.button`
+    color:grey;
+    font-size:10px;
+    paddingBottom:7px;
+`;
 
 class Answers extends React.Component {
     constructor() {
@@ -16,20 +39,39 @@ class Answers extends React.Component {
         else this.setState({clicked: true})
     }
     render() {
+        const {title,qustionId, checkedAnwser, originalAnswer} = this.props.answer||"TEST";
         const renderedList = this.props.answersList.map((answer,index) =>{
             return (
-                    <Answer key={index} index={index+1} answer={answer}/>
+                <>
+                    <Grid >
+                        <Row>
+                            {index+1}
+                        </Row>
+    
+                        <Row> 
+                            {title||`Question ${index+1}`}  >
+                        </Row>
+                        <Row> 
+                            {checkedAnwser===originalAnswer ? 
+                                <FiCheckCircle color="#33CCFF"/>:
+                                <FaRegTimesCircle color="#FF6347"/>}
+                            <FaRegTimesCircle color="#FF6347"/>
+                        </Row>
+                    </Grid>
+                </>
             );
         });
     
-        return (<>
-        
-            <div onClick={ this.onClick} style={{color :"grey", fontSize : "10px", paddingBottom : "7px"}}> {this.state.clicked ? "Hide your answers ▲":"See your answers ▼"}</div>
-            {this.state.clicked ? renderedList: ""}
+        return (
+            <>
+                <ShowAnswerList onClick={ this.onClick}>
+                    {this.state.clicked ? "Hide your answers ▲":"See your answers ▼"}
+                </ShowAnswerList>
+                {this.state.clicked ? renderedList: ""}
             
             </>
         )
-    }
+    };
 }
 
 export default Answers;
